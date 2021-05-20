6bef850a66ab1d41ea71dd71a93132dcaaf8a3ff
makl <kl.ma@tuya.com>
2021-05-20 15:12:53 +0800

    eureka demo

---
+++ server-a/src/main/java/org/mkl/servera/ServerAApplication.java
@@ -0,0 +1,15 @@
+package org.mkl.servera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class ServerAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerAApplication.class, args);
+	}
+
+}
---
+++ server-a/src/test/java/org/mkl/servera/ServerAApplicationTests.java
@@ -0,0 +1,13 @@
+// package org.mkl.servera;
+//
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
+//
+// @SpringBootTest
+// class ServerAApplicationTests {
+//
+// 	@Test
+// 	void contextLoads() {
+// 	}
+//
+// }
